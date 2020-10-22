e518efbd4d7e02bd5bd6437bec46c871929f42dc
Ellis Thompson <ellisthompson.1999@outlook.com>
2020-10-22 12:54:49 +0100

    Update create_channels.js

--- commands/admin/create_channels.js
+++ commands/admin/create_channels.js
@@ -13,7 +13,6 @@ module.exports = class CreateChannel extends Command {
 			description: 'Add all channels to the discord server',
             guildOnly: true,
             clientPermissions: ['ADMINISTRATOR'],
-            hidden = true,
 		});
     }
     
